9e596b8553bc14b1c039042c657d6232b8932772
stardream96 <stardream96@gmail.com>
2019-09-17 11:54:15 -0400

    finished json & text process module
    text extractor for pulled json file
    with example output text.txt
    waiting for any requirement for the next module and it may need to be modified

---
+++ text_extractor.py
@@ -0,0 +1,31 @@
+
+#author Linfeng
+'''
+This code take the local json file and output it to a list 
+of text for each tweets and write the result to an txt file
+'''
+
+import json    
+
+tweets = ['']
+tweets_dict_list = []
+tweet_text_lists_https_rmv = []
+with open('tweet.json') as json_file:
+	ind = 0
+	for line in json_file:
+		if line[0:2] == '}{':
+			tweets[ind]+='}'
+			tweets.append('')
+			ind+=1
+			tweets[ind]+='{'
+		else:
+			tweets[ind]+=line
+	for item in tweets:
+		tt = json.loads(item)
+		tweets_dict_list.append(tt)
+		tweet_text_lists_https_rmv.append(tt['text'].split('https')[0])
+	#print(tweet_text_lists_https_rmv)
+f = open('text.txt','w')
+for item in tweet_text_lists_https_rmv:
+	f.write(item+'\n')
+f.close()
