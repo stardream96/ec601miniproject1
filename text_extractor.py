5bf0c27aea26aee55c29c8377d41e28010bf81d2
stardream96 <stardream96@gmail.com>
2019-09-30 12:48:53 -0400

    updated

--- text_extractor.py
+++ text_extractor.py
@@ -7,25 +7,29 @@ of text for each tweets and write the result to an txt file
 
 import json    
 
-tweets = ['']
-tweets_dict_list = []
-tweet_text_lists_https_rmv = []
-with open('tweet.json') as json_file:
-	ind = 0
-	for line in json_file:
-		if line[0:2] == '}{':
-			tweets[ind]+='}'
-			tweets.append('')
-			ind+=1
-			tweets[ind]+='{'
-		else:
-			tweets[ind]+=line
-	for item in tweets:
-		tt = json.loads(item)
-		tweets_dict_list.append(tt)
-		tweet_text_lists_https_rmv.append(tt['text'].split('https')[0])
-	#print(tweet_text_lists_https_rmv)
-f = open('text.txt','w')
-for item in tweet_text_lists_https_rmv:
-	f.write(item+'\n')
-f.close()
+def text_extractor():
+	tweets = ['']
+	tweets_dict_list = []
+	tweet_text_lists_https_rmv = []
+	print("reading json...")
+	with open('tweet.json') as json_file:
+		ind = 0
+		for line in json_file:
+			if line[0:2] == '}{':
+				tweets[ind]+='}'
+				tweets.append('')
+				ind+=1
+				tweets[ind]+='{'
+			else:
+				tweets[ind]+=line
+		for item in tweets:
+			tt = json.loads(item)
+			tweets_dict_list.append(tt)
+			tweet_text_lists_https_rmv.append(tt['text'].split('https')[0])
+		#print(tweet_text_lists_https_rmv)
+	f = open('text.txt','w')
+	print("writing to file")
+	for item in tweet_text_lists_https_rmv:
+		f.write(item+'\n')
+	f.close()
+	return tweet_text_lists_https_rmv
--- tweetAPIexample.py
+++ tweetAPIexample.py
@@ -5,6 +5,7 @@
 
 import tweepy #https://github.com/tweepy/tweepy
 import json
+import text_extractor
 
 
 #Twitter API credentials
@@ -22,12 +23,12 @@ def get_all_tweets(screen_name):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
-    
+    num = 10
     #initialize a list to hold all the tweepy Tweets
     alltweets = []    
     
     #make initial request for most recent tweets (200 is the maximum allowed count)
-    new_tweets = api.user_timeline(screen_name = screen_name,count=10)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=num)
     
     #save most recent tweets
     alltweets.extend(new_tweets)
@@ -39,16 +40,16 @@ def get_all_tweets(screen_name):
     while len(new_tweets) > 0:
         
         #all subsiquent requests use the max_id param to prevent duplicates
-        new_tweets = api.user_timeline(screen_name = screen_name,count=10,max_id=oldest)
+        new_tweets = api.user_timeline(screen_name = screen_name,count=num,max_id=oldest)
         
         #save most recent tweets
         alltweets.extend(new_tweets)
         
         #update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        if(len(alltweets) > 15):
+        if(len(alltweets) > 100):#set total tweets grabed here.
             break
-        print('...%s tweets downloaded so far" %',len(alltweets))
+        print("...%s tweets downloaded so far" % len(alltweets))
        
     #write tweet objects to JSON
     file = open('tweet.json', 'w') 
@@ -62,4 +63,5 @@ def get_all_tweets(screen_name):
 
 if __name__ == '__main__':
     #pass in the username of the account you want to download
-    get_all_tweets("@Ibra_official")
+    get_all_tweets("@realDonaldTrump")
+    text_extractor.text_extractor()
